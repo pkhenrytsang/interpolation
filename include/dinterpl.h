15a1e63d5317d846250bfea116e489e1d7d4340b
pkhenrytsang <56750757+pkhenrytsang@users.noreply.github.com>
2019-10-21 15:13:05 -0400

    Add files via upload

--- include/dinterpl.h
+++ include/dinterpl.h
@@ -13,41 +13,25 @@ using namespace std;
 class dinterpl{
 
   public:
-  
-    /* evaluation cache */
-    typedef struct {
-    size_t  cache;        /* cache of index   */
-    size_t  cache_hit;   /* keep statistics  */
-    size_t  cache_miss;
-    }cache;
-    
-    static cache * cache_alloc (void)
-    {
-      cache *a = (cache *) malloc (sizeof (cache));
-
-      a->cache = 0;
-      a->cache_hit = 0;
-      a->cache_miss = 0;
-
-      return a;
-    }
-    static void cache_free (cache * a)
-    {
-      free (a);
-    }
 
     //Class constructor/destructor
     dinterpl(const double x_array[], const double y_array[], size_t size);
     ~dinterpl();
     
     //Interpolation Evaluation
-    double linear_eval(double x, cache * cache);
-    double cspline_eval(double x, cache * cache);
-    
     double linear_eval(double x);
     double cspline_eval(double x);
+
+    //Cached version
+    double linear_cached_eval(double x);
+    double cspline_cached_eval(double x);
+
+    //Static Version of linear interpolation
+    static double linear_eval (double x, const double x_array[], const double y_array[], size_t size);
+    static double linear_cached_eval (double x, const double x_array[], const double y_array[], size_t size);
     
     //Misc Routines
+    void cspline_filter(double threshold);
     void uniformize_grid(size_t s_size);
     
 
@@ -78,25 +62,6 @@ class dinterpl{
         max = x > x_array[i] ? max : i;
       }
       return min;
-    }
-
-    inline static size_t interp_accel_find(cache * cache, const double xa[], size_t len, double x)
-    {
-      size_t x_index = cache->cache;
-
-      if(x < xa[x_index]) {
-        cache->cache = interp_bsearch(xa, x, 0, x_index);
-        cache->cache_miss++;
-      }
-      else if(x >= xa[x_index + 1]) {
-        cache->cache = interp_bsearch(xa, x, x_index, len-1);
-        cache->cache_miss++;
-      }
-      else {
-        cache->cache_hit++;
-      }
-
-      return cache->cache;
     }
 
     inline static size_t interp_accel_find(size_t internal_cache, const double xa[], size_t len, double x)
